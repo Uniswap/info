9f86ccad70dfd5e68f6643d4ed14c9aa53009c7a
Jeff Reiner <theundeadghost@gmail.com>
2019-01-28 23:52:11 +0100

    :sparkles: build userpool state container

---
+++ src/containers/poolContainer.js
@@ -0,0 +1,27 @@
+import { Container } from "unstated";
+
+import { BASE_URL, Big } from "../helpers";
+
+export class PoolContainer extends Container {
+  state = {
+    userNumPoolTokens: 0,
+    userPoolPercent: 0
+  };
+
+  async fetchUser(exchangeAddress, userAccount) {
+    try {
+      const data = await fetch(
+        `${BASE_URL}v1/user?exchangeAddress=${exchangeAddress}&userAddress=${userAccount}`
+      );
+
+      const json = await data.json();
+
+      this.setState({
+        userNumPoolTokens: Big(json.userNumPoolTokens).toFixed(4),
+        userPoolPercent: (json.userPoolPercent * 100).toFixed(2)
+      });
+    } catch (err) {
+      console.log("error: ", err);
+    }
+  }
+}
