0b3b496b5c274f39c83245c87befd48b092b933c
David Kajpust <kajpustd@gmail.com>
2019-06-04 10:20:41 -0400

    Top ten exchanges looking really good

--- src/components/ExchangeTable/index.js
+++ src/components/ExchangeTable/index.js
@@ -1,81 +1,118 @@
-import React from "react";
-import { Box, Flex, Text } from "rebass";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react'
+import { Box, Text, Flex } from 'rebass'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import FourByFour from '../FourByFour'
-import { Hint } from '../index'
-import Panel from '../Panel'
+import { Divider, Hint } from '../index'
+import Loader from '../Loader'
 
-// TODO - gonna have to query top twenty exchanges, and then all their histories from 20 pages ago
-// Might shrink it down to 10 if the query is too long
-
-const PoolSizeItem = ({ topTen }) => (
-  <Flex  mb={24} alignItems="center" justifyContent='center'>
-    <Flex alignItems="center" pr={24}>
-        {topTen.tokenName}
-    </Flex>
-    <Text fontSize={[12, 16]}>
-      Total Liquidty in ETH: {topTen.tradeVolumeEth}
-    </Text>
-  </Flex>
-);
-
-const PoolSizeItem2 = ({ topTen }) => (
+const FirstExchangeItem = ({ topTen }) => (
   <FourByFour
     p={24}
-    alignItems="center" justifyContent='center'
     rounded bg="white"
-    topLeft={<Hint color="text">Token</Hint>}
-    width={1/3}
+    topLeft={
+      <Hint
+        color="text"
+        fontSize={15}
+        fontWeight={500}
+      >
+        Token
+      </Hint>}
     bottomLeft={
       <Text
         color="uniswappink"
         className="-transition"
         fontSize={20}
-        lineHeight={1.4}
-        fontWeight={500}
+        lineHeight={0.1}
+        fontWeight={350}
       >
         {topTen.tokenName}
       </Text>
     }
-    topRight={<Hint color="text">Total Liquidity In Eth</Hint>}
+    topRight={
+      <Hint
+        color="text"
+        fontSize={15}
+        fontWeight={500}
+      >
+        Total Volume In Eth
+      </Hint>}
     bottomRight={
       <Text
         color="uniswappink"
         fontSize={20}
-        lineHeight={1.4}
-        fontWeight={500}
+        lineHeight={0.1}
+        fontWeight={350}
       >
         {topTen.tradeVolumeEth}
       </Text>
     }
   />
-);
+)
 
+const ExchangeItem = ({ topTen }) => (
+  <Box>
+    <FourByFour
+      p={24}
+      rounded bg="white"
+      bottomLeft={
+        <Text
+          color="uniswappink"
+          className="-transition"
+          fontSize={20}
+          lineHeight={0}
+          fontWeight={250}
+        >
+          {topTen.tokenName}
+        </Text>
+      }
+      bottomRight={
+        <Text
+          color="uniswappink"
+          fontSize={20}
+          lineHeight={0}
+          fontWeight={250}
+        >
+          {topTen.tradeVolumeEth}
+        </Text>
+      }
+    />
 
+    <Divider/>
+  </Box>
+)
 
 const List = styled(Box)`
   height: 700px;
   max-height: 700px;
   overflow-y: scroll;
   -webkit-overflow-scrolling: touch;
-`;
+`
 
 // @TODO rework into virtualized list
-const PoolSizeList = ({ topTen }) => (
-  <List p={24}>
-    {topTen.slice(0, 20).map((exchanges, index) => (
-      <PoolSizeItem2 key={index} topTen={exchanges} />
-    ))}
-  </List>
-);
+const PoolSizeList = ({ topTen }) => {
+  if (topTen.length == 0) {
+    return (
+      <Loader/>
+    )
+  } else {
+    return (
+      <List p={24}>
+        <FirstExchangeItem topTen={topTen.slice(0, 1)}/>
+        {topTen.slice(1, 20).map((exchanges, index) => (
+          <ExchangeItem key={index} topTen={exchanges}/>
+        ))}
+      </List>
+    )
+  }
+}
 
 PoolSizeList.defaultProps = {
   transactions: []
-};
+}
 
 PoolSizeList.propTypes = {
   transactions: PropTypes.array.isRequired
-};
+}
 
-export default PoolSizeList;
+export default PoolSizeList
--- src/containers/overviewPageContainer.js
+++ src/containers/overviewPageContainer.js
@@ -73,6 +73,7 @@ export class OverviewPageContainer extends Container {
       })
       console.log(`fetched ${addresses.length} exchanges 24 hour trade volume`)
 
+      console.log(addresses)
       await this.setState({
         topTen: addresses
       })
