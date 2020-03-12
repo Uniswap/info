98242bd8f81a7f81c7380b4cd8110abf68b88e8f
ianlapham <ianlapham@gmail.com>
2020-03-12 14:07:31 -0400

    add warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { ApolloProvider } from 'react-apollo'
 import { client } from './apollo/client'
 import { Route, Switch, BrowserRouter, withRouter, Redirect } from 'react-router-dom'
@@ -12,7 +13,22 @@ import { timeframeOptions } from './constants'
 import { useAllExchanges } from './Data/GetAllExchanges'
 import LocalLoader from './components/LocalLoader'
 
-function App(props) {
+const WarningWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+const WarningBanner = styled.div`
+  background-color: #ff6871;
+  padding: 1.5rem;
+  color: white;
+  width: 100%;
+  text-align: center;
+  font-weight: 500;
+`
+
+function App() {
   // set default time box to all time
   const [historyDaysToQuery, setHistoryDaysToQuery] = useState(timeframeOptions[3].value)
 
@@ -59,6 +75,11 @@ function App(props) {
   return (
     <ApolloProvider client={client}>
       <Wrapper>
+        <WarningWrapper>
+          <WarningBanner>
+            Warning: The data on this site has not been updated since 03/09/20. Please check back soon.
+          </WarningBanner>
+        </WarningWrapper>
         <div style={{ position: 'relative' }}>
           {globalData && uniswapHistory && length > 0 ? (
             <BrowserRouter>
