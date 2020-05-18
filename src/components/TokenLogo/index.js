2df29d17d0c71584b941467cfa045dc0323ef783
ianlapham <ianlapham@gmail.com>
2020-05-18 14:39:19 -0400

    volume bug fix

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -46,15 +46,15 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
   }
 
   // hard coded fixes for trust wallet api issues
-  if (address.toLowerCase() === '0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb') {
+  if (address?.toLowerCase() === '0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb') {
     address = '0x42456d7084eacf4083f1140d3229471bba2949a8'
   }
 
-  if (address.toLowerCase() === '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f') {
+  if (address?.toLowerCase() === '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f') {
     address = '0xc011a72400e58ecd99ee497cf89e3775d4bd732f'
   }
 
-  if (address.toLowerCase() === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') {
+  if (address?.toLowerCase() === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') {
     return (
       <StyledEthereumLogo size={size} {...rest}>
         <img
