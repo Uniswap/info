2a55f8b6e20e04322ac0e6a4fc2e575d2175a9bf
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-05-18 12:58:20 -0400

    Add migration banner

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -60,6 +60,33 @@ const NavLeft = styled.div`
   align-items: center;
 `
 
+const MigrateBanner = styled.div`
+  width: 100%;
+  padding: 12px 0;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.2);
+  color: rgba(255, 255, 255, 1);
+  font-weight: 400;
+  text-align: center;
+  a {
+    color: rgba(255, 255, 255, 1);
+    text-decoration: underline;
+  }
+`
+
+const MigrateBannerSmall = styled(MigrateBanner)`
+  @media (min-width: 960px) {
+    display: none;
+  }
+`
+
+const MigrateBannerLarge = styled(MigrateBanner)`
+  @media (max-width: 960px) {
+    display: none;
+  }
+`
+
 export default function NavHeader({ exchanges, setCurrencyUnit, currencyUnit }) {
   // for now exclude broken tokens
   const [filteredDirectory, setDirectory] = useState([])
@@ -91,26 +118,38 @@ export default function NavHeader({ exchanges, setCurrencyUnit, currencyUnit }) 
   const history = useHistory()
 
   return (
-    <Header bg={['transparent', 'transparent']}>
-      <NavLeft>
-        <Title />
-        <LinkText to="/" selected={window.location.pathname === '/home'}>
-          Back to Home
-        </LinkText>
-      </NavLeft>
-      <NavRight>
-        <CurrencySelect setCurrencyUnit={setCurrencyUnit} currencyUnit={currencyUnit} />
-        <TokenSelect
-          options={filteredDirectory}
-          setCapEth={setCapEth}
-          capEth={capEth}
-          tokenSelect={true}
-          placeholder={belowLarge ? 'Tokens' : 'Find token'}
-          onChange={select => {
-            history.push('/token/' + select.tokenAddress)
-          }}
-        />
-      </NavRight>
-    </Header>
+    <>
+      <MigrateBannerSmall>
+        <b>V2 is here!&nbsp;</b> <Link href="https://migrate.uniswap.exchange/">Migrate your liquidity&nbsp;</Link>or{' '}
+        <Link href="https://uniswap.exchange"> &nbsp;use V2 ↗</Link>
+      </MigrateBannerSmall>
+      <MigrateBannerLarge>
+        This site displays analytics for Uniswap V1 only. To see Uniswap V2 analytics&nbsp;
+        <Link href="https://uniswap.info">
+          <b>click here ↗</b>
+        </Link>
+      </MigrateBannerLarge>
+      <Header bg={['transparent', 'transparent']}>
+        <NavLeft>
+          <Title />
+          <LinkText to="/" selected={window.location.pathname === '/home'}>
+            Back to Home
+          </LinkText>
+        </NavLeft>
+        <NavRight>
+          <CurrencySelect setCurrencyUnit={setCurrencyUnit} currencyUnit={currencyUnit} />
+          <TokenSelect
+            options={filteredDirectory}
+            setCapEth={setCapEth}
+            capEth={capEth}
+            tokenSelect={true}
+            placeholder={belowLarge ? 'Tokens' : 'Find token'}
+            onChange={select => {
+              history.push('/token/' + select.tokenAddress)
+            }}
+          />
+        </NavRight>
+      </Header>
+    </>
   )
 }
