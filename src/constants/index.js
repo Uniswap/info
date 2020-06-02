64709748e9bbfe6a361972ff7a0fa789039d4d49
Ian Lapham <ianlapham@gmail.com>
2020-06-01 21:48:41 -0400

    add pair whitelist for overview page (#183)
    * add pair whitelist for overview page
    
    * update sorting

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,3 +9,13 @@ export const timeframeOptions = {
   // YEAR: '1 year',
   ALL_TIME: 'All time'
 }
+
+export const OVERVIEW_WHITELIST = [
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', //WETH
+  '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // usdc
+  '0xdac17f958d2ee523a2206206994597c13d831ec7', // usdt
+  '0x0000000000085d4780b73119b644ae5ecd22b376', //tusd
+  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', //cdai
+  '0x39aa39c021dfbae8fac545936693ac917d5e7563' // cusdc
+]
