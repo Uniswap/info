006c704fc738ff304eec485a2c42b0db73739ab2
ianlapham <ianlapham@gmail.com>
2020-06-10 20:06:04 -0400

    update with official subgraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-local'
+    uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
