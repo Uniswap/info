b7c70b9a30a25dd746c7182ce4cb4d5952f40a8a
ianlapham <ianlapham@gmail.com>
2020-06-14 22:44:50 -0400

    update v1 client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -12,7 +12,7 @@ export const client = new ApolloClient({
 
 export const v1Client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapbackup'
+    uri: 'https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
