c5b76201287aafaa9a3da12931cfcccf6cc5cdc9
Ian Lapham <ianlapham@gmail.com>
2020-06-22 12:22:29 -0700

    add more precision to formatted nums (#214)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -172,7 +172,7 @@ export const formattedNum = (number, usd = false) => {
     }
   }
 
-  return Number(parseFloat(num).toFixed(4))
+  return Number(parseFloat(num).toFixed(5))
 }
 
 export function formattedPercent(percent) {
