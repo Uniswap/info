c5af69506c05439a74e001031e896c87205c786c
Ian Lapham <ianlapham@Ians-MacBook-Pro.local>
2020-07-20 13:11:26 -0400

    update token all pair fetching to sort by USD reserve

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -689,10 +689,10 @@ export const TOKEN_DATA = (tokenAddress, block) => {
       tokens(${block ? `block : {number ${block}}` : ``} where: {id:"${tokenAddress}"}) {
         ...TokenFields
       }
-      pairs0: pairs(where: {token0: "${tokenAddress}"}){
+      pairs0: pairs(where: {token0: "${tokenAddress}"}, first: 100, orderBy: reserveUSD, orderDirection: desc){
         id
       }
-      pairs1: pairs(where: {token1: "${tokenAddress}"}){
+      pairs1: pairs(where: {token1: "${tokenAddress}"}, , first: 100, orderBy: reserveUSD, orderDirection: desc){
         id
       }
     }
