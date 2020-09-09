d172535fe728bd09ea8b440856cec8e363fe5172
Oliver Nordbjerg <hi@notbjerg.me>
2020-09-09 22:43:24 +0200

    Use lower case factory address (The Graph smh)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const FACTORY_ADDRESS = '0xA818b4F111Ccac7AA31D0BCc0806d64F2E0737D7'
+export const FACTORY_ADDRESS = '0xa818b4f111ccac7aa31d0bcc0806d64f2e0737d7'
 
 export const BUNDLE_ID = '1'
 
