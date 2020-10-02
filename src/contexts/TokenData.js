978768774db81d3853d1f889888d8dd4ceb154d2
Kirill <krboktv@gmail.com>
2020-10-03 00:49:01 +0300

    update token list

--- src/contexts/TokenData.js
+++ src/contexts/TokenData.js
@@ -399,7 +399,7 @@ const getTokenTransactions = async allPairsFormatted => {
 const getVerifiedTokens = async () => {
   try {
     // const res = await fetch('http://tokens.1inch.eth.link');
-    const res = await fetch('https://gateway.ipfs.io/ipfs/QmakFV6sBJaWQ11pY7juVjro6GfueQqqQv7Lxc5aGqMPZg');
+    const res = await fetch('https://gateway.ipfs.io/ipfs/QmPUMPthBuZ18KfXGnjnqVXTon2dQusequZZ81yxNEv5yZ');
     const tokens = (await res.json()).tokens.map((x) => x.address.toLowerCase());
     tokens.push(ETH);
     return tokens;
