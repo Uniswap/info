e4481653c31520ffb0103d68da17e0a8210f9bf2
ianlapham <ianlapham@gmail.com>
2020-10-05 15:50:55 -0400

    remove web3

--- src/contexts/Application.js
+++ src/contexts/Application.js
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useReducer, useMemo, useCallback, useState, useEffect } from 'react'
 import { timeframeOptions, SUPPORTED_LIST_URLS__NO_ENS } from '../constants'
-import Web3 from 'web3'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import getTokenList from '../utils/tokenLists'
@@ -11,7 +10,6 @@ dayjs.extend(utc)
 const UPDATE = 'UPDATE'
 const UPDATE_TIMEFRAME = 'UPDATE_TIMEFRAME'
 const UPDATE_SESSION_START = 'UPDATE_SESSION_START'
-const UPDATE_WEB3 = 'UPDATE_WEB3'
 const UPDATED_SUPPORTED_TOKENS = 'UPDATED_SUPPORTED_TOKENS'
 const UPDATE_LATEST_BLOCK = 'UPDATE_LATEST_BLOCK'
 
@@ -19,7 +17,6 @@ const SUPPORTED_TOKENS = 'SUPPORTED_TOKENS'
 const TIME_KEY = 'TIME_KEY'
 const CURRENCY = 'CURRENCY'
 const SESSION_START = 'SESSION_START'
-const WEB3 = 'WEB3'
 const LATEST_BLOCK = 'LATEST_BLOCK'
 
 const ApplicationContext = createContext()
@@ -34,28 +31,21 @@ function reducer(state, { type, payload }) {
       const { currency } = payload
       return {
         ...state,
-        [CURRENCY]: currency,
+        [CURRENCY]: currency
       }
     }
     case UPDATE_TIMEFRAME: {
       const { newTimeFrame } = payload
       return {
         ...state,
-        [TIME_KEY]: newTimeFrame,
+        [TIME_KEY]: newTimeFrame
       }
     }
     case UPDATE_SESSION_START: {
       const { timestamp } = payload
       return {
         ...state,
-        [SESSION_START]: timestamp,
-      }
-    }
-    case UPDATE_WEB3: {
-      const { web3 } = payload
-      return {
-        ...state,
-        [WEB3]: web3,
+        [SESSION_START]: timestamp
       }
     }
 
@@ -63,7 +53,7 @@ function reducer(state, { type, payload }) {
       const { block } = payload
       return {
         ...state,
-        [LATEST_BLOCK]: block,
+        [LATEST_BLOCK]: block
       }
     }
 
@@ -71,7 +61,7 @@ function reducer(state, { type, payload }) {
       const { supportedTokens } = payload
       return {
         ...state,
-        [SUPPORTED_TOKENS]: supportedTokens,
+        [SUPPORTED_TOKENS]: supportedTokens
       }
     }
 
@@ -83,64 +73,55 @@ function reducer(state, { type, payload }) {
 
 const INITIAL_STATE = {
   CURRENCY: 'USD',
-  TIME_KEY: timeframeOptions.ALL_TIME,
+  TIME_KEY: timeframeOptions.ALL_TIME
 }
 
 export default function Provider({ children }) {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
-  const update = useCallback((currency) => {
+  const update = useCallback(currency => {
     dispatch({
       type: UPDATE,
       payload: {
-        currency,
-      },
+        currency
+      }
     })
   }, [])
 
   // global time window for charts - see timeframe options in constants
-  const updateTimeframe = useCallback((newTimeFrame) => {
+  const updateTimeframe = useCallback(newTimeFrame => {
     dispatch({
       type: UPDATE_TIMEFRAME,
       payload: {
-        newTimeFrame,
-      },
+        newTimeFrame
+      }
     })
   }, [])
 
   // used for refresh button
-  const updateSessionStart = useCallback((timestamp) => {
+  const updateSessionStart = useCallback(timestamp => {
     dispatch({
       type: UPDATE_SESSION_START,
       payload: {
-        timestamp,
-      },
+        timestamp
+      }
     })
   }, [])
 
-  const updateWeb3 = useCallback((web3) => {
-    dispatch({
-      type: UPDATE_WEB3,
-      payload: {
-        web3,
-      },
-    })
-  }, [])
-
-  const updateSupportedTokens = useCallback((supportedTokens) => {
+  const updateSupportedTokens = useCallback(supportedTokens => {
     dispatch({
       type: UPDATED_SUPPORTED_TOKENS,
       payload: {
-        supportedTokens,
-      },
+        supportedTokens
+      }
     })
   }, [])
 
-  const updateLatestBlock = useCallback((block) => {
+  const updateLatestBlock = useCallback(block => {
     dispatch({
       type: UPDATE_LATEST_BLOCK,
       payload: {
-        block,
-      },
+        block
+      }
     })
   }, [])
 
@@ -153,12 +134,11 @@ export default function Provider({ children }) {
             update,
             updateSessionStart,
             updateTimeframe,
-            updateWeb3,
             updateSupportedTokens,
-            updateLatestBlock,
-          },
+            updateLatestBlock
+          }
         ],
-        [state, update, updateTimeframe, updateWeb3, updateSessionStart, updateSupportedTokens, updateLatestBlock]
+        [state, update, updateTimeframe, updateSessionStart, updateSupportedTokens, updateLatestBlock]
       )}
     >
       {children}
@@ -175,7 +155,7 @@ export function useLatestBlock() {
     async function fetch() {
       try {
         const res = await healthClient.query({
-          query: SUBGRAPH_HEALTH,
+          query: SUBGRAPH_HEALTH
         })
         const block = res.data.indexingStatusForCurrentVersion.chains[0].latestBlock.number
         if (block) {
@@ -258,24 +238,6 @@ export function useSessionStart() {
   return parseInt(seconds / 1000)
 }
 
-/**
- * @todo this isnt used now - if ever needed probably better to use
- * web3-react instead of this custom hook
- */
-export function useWeb3() {
-  const [state, { updateWeb3 }] = useApplicationContext()
-  const web3 = state?.[WEB3]
-
-  useEffect(() => {
-    if (!web3) {
-      const web3 = new Web3(new Web3.providers.HttpProvider(process.env.REACT_APP_NETWORK_URL))
-      updateWeb3(web3)
-    }
-  })
-
-  return web3
-}
-
 export function useListedTokens() {
   const [state, { updateSupportedTokens }] = useApplicationContext()
   const supportedTokens = state?.[SUPPORTED_TOKENS]
@@ -287,7 +249,7 @@ export function useListedTokens() {
         const newTokens = await getTokenList(url)
         return Promise.resolve([...tokensSoFar, ...newTokens.tokens])
       }, Promise.resolve([]))
-      let formatted = allFetched?.map((t) => t.address.toLowerCase())
+      let formatted = allFetched?.map(t => t.address.toLowerCase())
       updateSupportedTokens(formatted)
     }
     if (!supportedTokens) {
