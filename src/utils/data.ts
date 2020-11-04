c46e2cd0d233648113b0d58ae099b90493150a97
Ian Lapham <ianlapham@gmail.com>
2020-11-04 12:37:28 -0500

    Update data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -13,7 +13,7 @@ interface BasicData {
 
 // Override data return from graph - usually because proxy token has changed
 // names since entitiy was created in subgraph
-// keys are token addresses
+// keys are lowercase token addresses <--------
 const TOKEN_OVERRIDES: { [address: string]: { name: string; symbol: string } } = {
   '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': {
     name: 'Ether (Wrapped)',
