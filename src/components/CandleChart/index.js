ed2d2066bb90ead78f58f201a01aa929a20aae8f
Lint Action <lint-action@samuelmeuli.com>
2021-01-19 23:29:42 +0000

    Fix code style issues with ESLint

--- src/components/CandleChart/index.js
+++ src/components/CandleChart/index.js
@@ -31,18 +31,18 @@ const CandleStickChart = ({
   height = 300,
   base,
   margin = true,
-  valueFormatter = val => formattedNum(val, true)
+  valueFormatter = (val) => formattedNum(val, true),
 }) => {
   // reference for DOM element to create with chart
   const ref = useRef()
 
-  const formattedData = data?.map(entry => {
+  const formattedData = data?.map((entry) => {
     return {
       time: parseFloat(entry.timestamp),
       open: parseFloat(entry.open),
       low: parseFloat(entry.open),
       close: parseFloat(entry.close),
-      high: parseFloat(entry.close)
+      high: parseFloat(entry.close),
     }
   })
 
@@ -52,7 +52,7 @@ const CandleStickChart = ({
       open: parseFloat(formattedData[formattedData.length - 1].close),
       close: parseFloat(base),
       low: Math.min(parseFloat(base), parseFloat(formattedData[formattedData.length - 1].close)),
-      high: Math.max(parseFloat(base), parseFloat(formattedData[formattedData.length - 1].close))
+      high: Math.max(parseFloat(base), parseFloat(formattedData[formattedData.length - 1].close)),
     })
   }
 
@@ -95,29 +95,29 @@ const CandleStickChart = ({
         height: height,
         layout: {
           backgroundColor: 'transparent',
-          textColor: textColor
+          textColor: textColor,
         },
         grid: {
           vertLines: {
-            color: 'rgba(197, 203, 206, 0.5)'
+            color: 'rgba(197, 203, 206, 0.5)',
           },
           horzLines: {
-            color: 'rgba(197, 203, 206, 0.5)'
-          }
+            color: 'rgba(197, 203, 206, 0.5)',
+          },
         },
         crosshair: {
-          mode: CrosshairMode.Normal
+          mode: CrosshairMode.Normal,
         },
         rightPriceScale: {
           borderColor: 'rgba(197, 203, 206, 0.8)',
-          visible: true
+          visible: true,
         },
         timeScale: {
-          borderColor: 'rgba(197, 203, 206, 0.8)'
+          borderColor: 'rgba(197, 203, 206, 0.8)',
         },
         localization: {
-          priceFormatter: val => formattedNum(val)
-        }
+          priceFormatter: (val) => formattedNum(val),
+        },
       })
 
       var candleSeries = chart.addCandlestickSeries({
@@ -126,7 +126,7 @@ const CandleStickChart = ({
         borderDownColor: 'red',
         borderUpColor: 'green',
         wickDownColor: 'red',
-        wickUpColor: 'green'
+        wickUpColor: 'green',
       })
 
       candleSeries.setData(formattedData)
@@ -149,7 +149,7 @@ const CandleStickChart = ({
       setLastBarText()
 
       // update the title when hovering on the chart
-      chart.subscribeCrosshairMove(function(param) {
+      chart.subscribeCrosshairMove(function (param) {
         if (
           param === undefined ||
           param.time === undefined ||
