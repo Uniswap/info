4a7328500f20959444bf4fb4f1ba6224f84596ec
Hoang Trinh <hoang.trinhj@gmail.com>
2021-03-19 13:34:45 +0700

    https://kybernetwork.atlassian.net/browse/DMM-92 - Fix Fee In List Pair

--- src/components/PairList/index.js
+++ src/components/PairList/index.js
@@ -150,7 +150,7 @@ function PairList({ pairs, color, disbaleLinks, maxItems = 10 }) {
     if (pairData && pairData.token0 && pairData.token1) {
       const liquidity = formattedNum(pairData.reserveUSD, true)
       const volume = formattedNum(pairData.oneDayVolumeUSD, true)
-      const apy = formattedPercent((pairData.oneDayVolumeUSD * 0.003 * 365 * 100) / pairData.reserveUSD)
+      const apy = formattedPercent((pairData.oneDayFeeUSD * 365 * 100) / pairData.reserveUSD)
 
       return (
         <DashGrid style={{ height: '48px' }} disbaleLinks={disbaleLinks} focus={true}>
@@ -174,7 +174,7 @@ function PairList({ pairs, color, disbaleLinks, maxItems = 10 }) {
           <DataText area="liq">{liquidity}</DataText>
           <DataText area="vol">{volume}</DataText>
           {!below1080 && <DataText area="volWeek">{formattedNum(pairData.oneWeekVolumeUSD, true)}</DataText>}
-          {!below1080 && <DataText area="fees">{formattedNum(pairData.oneDayVolumeUSD * 0.003, true)}</DataText>}
+          {!below1080 && <DataText area="fees">{formattedNum(pairData.oneDayFeeUSD, true)}</DataText>}
           {!below1080 && <DataText area="apy">{apy}</DataText>}
         </DashGrid>
       )
