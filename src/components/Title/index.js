4319c13c1caff72fe5bc47855b9fab502c42520e
Hoang Trinh <hoang.trinhj@gmail.com>
2021-04-07 16:57:59 +0700

    https://kybernetwork.atlassian.net/browse/DMM-127 - Fix Mobile Header

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,42 +1,94 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
-
+import { useMedia } from 'react-use'
 import { Flex } from 'rebass'
-import Link from '../Link'
+
+import Link, { BasicLink } from '../Link'
 import { RowFixed } from '../Row'
 import Logo from '../../assets/logo_white.svg'
-import Wordmark from '../../assets/wordmark_white.svg'
 
 const TitleWrapper = styled.div`
   text-decoration: none;
-
+  z-index: 10;
+  width: 100%;
   &:hover {
     cursor: pointer;
   }
-
-  z-index: 10;
 `
 
-const UniIcon = styled(Link)`
+const DMMIcon = styled(Link)`
   transition: transform 0.3s ease;
   :hover {
     transform: rotate(-5deg);
   }
 `
 
+const Option = styled.div`
+  font-weight: 500;
+  font-size: 14px;
+  opacity: ${({ activeText }) => (activeText ? 1 : 0.6)};
+  color: ${({ theme }) => theme.white};
+  display: flex;
+  margin-left: 12px;
+  :hover {
+    opacity: 1;
+  }
+`
+
 export default function Title() {
   const history = useHistory()
+  const below1080 = useMedia('(max-width: 1080px)')
 
   return (
-    <TitleWrapper onClick={() => history.push('/')}>
-      <Flex alignItems="center">
+    <TitleWrapper>
+      <Flex alignItems="center" style={{ justifyContent: 'space-between' }}>
         <RowFixed>
-          <UniIcon id="link" onClick={() => history.push('/')}>
-            <img width={'100px'} src={Logo} alt="logo" />
-          </UniIcon>
-          {/* <img width={'84px'} style={{ marginLeft: '8px', marginTop: '0px' }} src={Wordmark} alt="logo" /> */}
+          <DMMIcon id="link" onClick={() => history.push('/')}>
+            <img width={below1080 ? '50px' : '100px'} src={Logo} alt="logo" />
+          </DMMIcon>
         </RowFixed>
+        {below1080 && (
+          <RowFixed style={{ alignItems: 'flex-end' }}>
+            <BasicLink to="/home">
+              <Option activeText={history.location.pathname === '/home' ?? undefined}>Summary</Option>
+            </BasicLink>
+            <BasicLink to="/tokens">
+              <Option
+                activeText={
+                  (history.location.pathname.split('/')[1] === 'tokens' ||
+                    history.location.pathname.split('/')[1] === 'token') ??
+                  undefined
+                }
+              >
+                Tokens
+              </Option>
+            </BasicLink>
+            <BasicLink to="/pairs">
+              <Option
+                activeText={
+                  (history.location.pathname.split('/')[1] === 'pairs' ||
+                    history.location.pathname.split('/')[1] === 'pair') ??
+                  undefined
+                }
+              >
+                Pairs
+              </Option>
+            </BasicLink>
+
+            <BasicLink to="/accounts">
+              <Option
+                activeText={
+                  (history.location.pathname.split('/')[1] === 'accounts' ||
+                    history.location.pathname.split('/')[1] === 'account') ??
+                  undefined
+                }
+              >
+                Wallet Data
+              </Option>
+            </BasicLink>
+          </RowFixed>
+        )}
       </Flex>
     </TitleWrapper>
   )
