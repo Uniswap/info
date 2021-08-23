a345c30225bc9405617255d9fda6d4780aae2dd9
Hoang Trinh <hoang.trinhj@gmail.com>
2021-08-23 11:16:17 +0700

    refactor: Update Token Logo

---
+++ src/constants/tokenLists/ethereum.tokenlist.js
@@ -0,0 +1,53 @@
+const ETHEREUM_TOKEN_LIST = {
+  '0xdd974D5C2e2928deA5F71b9825b8b646686BD200': {
+    address: '0xdd974D5C2e2928deA5F71b9825b8b646686BD200',
+    symbol: 'KNCL',
+    name: 'Kyber Network Crystal Legacy',
+    decimals: 18,
+    logoURI: 'https://raw.githubusercontent.com/dynamic-amm/dmm-interface/develop/src/assets/images/KNCL.png',
+  },
+  '0x0ccD5DD52Dee42B171a623478e5261C1eaaE092A': {
+    address: '0x0ccD5DD52Dee42B171a623478e5261C1eaaE092A',
+    symbol: 'DFM',
+    name: 'DFM',
+    decimals: 18,
+    logoURI: 'https://mycoldwalletnow.wpcomstaging.com/htdocs/wp-content/mcwhp/img/logo.svg',
+  },
+  '0x6e36556B3ee5Aa28Def2a8EC3DAe30eC2B208739': {
+    address: '0x6e36556B3ee5Aa28Def2a8EC3DAe30eC2B208739',
+    symbol: 'BUILD',
+    name: 'BUILD Finance',
+    decimals: 18,
+    logoURI: 'https://build.finance/images/logo.png',
+  },
+  '0x0bfEc35a1A3550Deed3F6fC76Dde7FC412729a91': {
+    address: '0x0bfEc35a1A3550Deed3F6fC76Dde7FC412729a91',
+    symbol: 'XKNCA',
+    name: 'xKNCa',
+    decimals: 18,
+    logoURI: 'https://assets.coingecko.com/coins/images/13995/thumb/Token-4.png?1613545763',
+  },
+  '0x6710c63432A2De02954fc0f851db07146a6c0312': {
+    address: '0x6710c63432A2De02954fc0f851db07146a6c0312',
+    symbol: 'MFG',
+    name: 'Smart MFG',
+    decimals: 18,
+    logoURI: 'https://i.imgur.com/oReNLqf.png',
+  },
+  '0x70e8dE73cE538DA2bEEd35d14187F6959a8ecA96': {
+    address: '0x70e8dE73cE538DA2bEEd35d14187F6959a8ecA96',
+    symbol: 'XSGD',
+    name: 'XSGD',
+    decimals: 6,
+    logoURI: 'https://assets.website-files.com/600e34cbaf525c42912af8b6/600e34cbaf525c7ad82af99b_xgd-img%20(1).svg',
+  },
+  '0xBCD515D6C5de70D3A31D999A7FA6a299657De294': {
+    address: '0xBCD515D6C5de70D3A31D999A7FA6a299657De294',
+    symbol: 'RICE',
+    name: 'RICE',
+    decimals: 18,
+    logoURI: 'https://drive.google.com/uc?export=download&id=16PVXI1Da5P27cywWiyqrcV-Q17u1aXsi',
+  },
+}
+
+export default ETHEREUM_TOKEN_LIST
