2c392d1186f9d084bc9b6914f986169865fd4e55
Nguyen Van Viet <vietnv1304@gmail.com>
2021-10-20 15:51:57 +0700

    feat: add fantom info (#196)
    * feat: add fantom info
    
    * update ci

--- src/components/SwitcNetworkButton/index.js
+++ src/components/SwitcNetworkButton/index.js
@@ -33,7 +33,7 @@ const SwitchNetworkButton = () => {
 
   const toggleNetworkModal = useToggleNetworkModal()
 
-  if (![1, 137, 56, 43114].includes(chainId)) {
+  if (![1, 137, 56, 43114, 250].includes(chainId)) {
     return null
   }
 
---
+++ src/constants/tokenLists/fantom.tokenlist.js
@@ -0,0 +1,39 @@
+const FANTOM_TOKEN_LIST = {
+  '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75':{
+      chainId: 250,
+      address: '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
+      symbol: 'USDC',
+      name: 'USDC',
+      decimals: 6
+    },
+  '0x049d68029688eAbF473097a2fC38ef61633A3C7A': {
+      chainId: 250,
+      address: '0x049d68029688eAbF473097a2fC38ef61633A3C7A',
+      symbol: 'fUSDT',
+      name: 'fUSDT',
+      decimals: 6
+    },
+  "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E":{
+      chainId: 250,
+      address: '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E',
+      symbol: 'DAI',
+      name: 'DAI',
+      decimals: 18
+    },
+  "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83":{
+      chainId: 250,
+      address: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
+      symbol: 'WFTM',
+      name: 'Wrapped FTM',
+      decimals: 18
+    },
+  "0x74b23882a30290451A17c44f4F05243b6b58C76d":{
+      chainId: 250,
+      address: '0x74b23882a30290451A17c44f4F05243b6b58C76d',
+      symbol: 'WETH',
+      name: 'Wrapped Ether',
+      decimals: 18
+    }
+}
+
+export default FANTOM_TOKEN_LIST
