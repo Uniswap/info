e90bf6bf37bb32414ee30abec4e6d54127d192ab
tungnt <tungnt@linecorp.com>
2021-10-26 17:26:54 +0700

    Update header link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Dropdown from "react-bootstrap/Dropdown";
-import LogoText from "../../assets/_logo.svg";
-import Burger from "../../assets/burger.svg";
-import Close from "../../assets/close.svg";
-import { CustomContainer, CustomNavbar, CustomNavbarToggle, CustomNavbarCollapse } from "./styled";
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Dropdown from 'react-bootstrap/Dropdown'
+import LogoText from '../../assets/_logo.svg'
+import Burger from '../../assets/burger.svg'
+import Close from '../../assets/close.svg'
+import { CustomContainer, CustomNavbar, CustomNavbarToggle, CustomNavbarCollapse } from './styled'
 
-const FactoryDomain = process.env.REACT_APP_FACCHAIN_DOMAIN;
-const SwapDomain = process.env.REACT_APP_SWAP_DOMAIN;
+const FactoryDomain = process.env.REACT_APP_FACCHAIN_DOMAIN
+const SwapDomain = process.env.REACT_APP_SWAP_DOMAIN
 
 export default function Header() {
   return (
@@ -33,7 +33,7 @@ export default function Header() {
               <Dropdown as={Nav.Item}>
                 <Dropdown.Toggle as={Nav.Link}>Transactions</Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href={FactoryDomain + "/txs"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/txs'} target="_blank">
                     All Transactions
                   </Dropdown.Item>
                 </Dropdown.Menu>
@@ -42,13 +42,13 @@ export default function Header() {
               <Dropdown as={Nav.Item}>
                 <Dropdown.Toggle as={Nav.Link}>Accounts</Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href={FactoryDomain + "/accounts"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/accounts'} target="_blank">
                     All Accounts
                   </Dropdown.Item>
-                  <Dropdown.Item href={FactoryDomain + "/validators"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/validators'} target="_blank">
                     All Validators
                   </Dropdown.Item>
-                  <Dropdown.Item href={FactoryDomain + "/contracts"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/contracts'} target="_blank">
                     Verified Contracts
                   </Dropdown.Item>
                 </Dropdown.Menu>
@@ -57,16 +57,16 @@ export default function Header() {
               <Dropdown as={Nav.Item}>
                 <Dropdown.Toggle as={Nav.Link}>Tokens</Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href={FactoryDomain + "/tokens/fac20"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/tokens/frc20'} target="_blank">
                     FRC-20 Tokens
                   </Dropdown.Item>
-                  <Dropdown.Item href={FactoryDomain + "/tokentxs"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/tokentxs'} target="_blank">
                     FRC-20 Transfers
                   </Dropdown.Item>
-                  <Dropdown.Item href={FactoryDomain + "/tokens/nft"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/tokens/nft'} target="_blank">
                     FRC721 Tokens
                   </Dropdown.Item>
-                  <Dropdown.Item href={FactoryDomain + "/tokentxs/nft"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/tokentxs/nft'} target="_blank">
                     FRC721 Transfers
                   </Dropdown.Item>
                 </Dropdown.Menu>
@@ -75,7 +75,7 @@ export default function Header() {
               <Dropdown as={Nav.Item}>
                 <Dropdown.Toggle as={Nav.Link}>Blocks</Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href={FactoryDomain + "/blocks"} target="_blank">
+                  <Dropdown.Item href={FactoryDomain + '/blocks'} target="_blank">
                     Blocks
                   </Dropdown.Item>
                 </Dropdown.Menu>
@@ -84,10 +84,10 @@ export default function Header() {
               <Dropdown as={Nav.Item}>
                 <Dropdown.Toggle as={Nav.Link}>Trade</Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href={SwapDomain + "/#/swap"} target="_blank">
+                  <Dropdown.Item href={SwapDomain + '/#/swap'} target="_blank">
                     Swap
                   </Dropdown.Item>
-                  <Dropdown.Item href={SwapDomain + "/#/pool/v2"} target="_blank">
+                  <Dropdown.Item href={SwapDomain + '/#/pool/v2'} target="_blank">
                     Liquidity
                   </Dropdown.Item>
                 </Dropdown.Menu>
@@ -97,5 +97,5 @@ export default function Header() {
         </CustomContainer>
       </CustomNavbar>
     </div>
-  );
+  )
 }
