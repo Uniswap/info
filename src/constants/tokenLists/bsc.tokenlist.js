5d50bf8c0163eee031e30639bb0e0d3657467cc9
viet-nv <vietnv1304@gmail.com>
2021-11-19 21:46:00 +0700

    feat: add ZUKI and EBA logo

--- src/constants/tokenLists/bsc.tokenlist.js
+++ src/constants/tokenLists/bsc.tokenlist.js
@@ -33,6 +33,30 @@ const BSC_TOKEN_LIST = {
     logoURI:
       'https://raw.githubusercontent.com/dforce-network/documents/2eea27372fda6a207f60fba4274ad68008d63694/logos/Lending/EUX.svg',
   },
+  '0xD6Cce248263ea1e2b8cB765178C944Fc16Ed0727': {
+    chainId: 56,
+    address: '0xD6Cce248263ea1e2b8cB765178C944Fc16Ed0727',
+    symbol: 'CTR',
+    name: 'Creator Chain',
+    decimals: 18,
+    logoURI: 'https://bscscan.com/token/images/creator_32.png',
+  },
+  '0x3944aC66b9b9B40a6474022D6962b6cAA001b5E3': {
+    chainId: 56,
+    address: '0x3944aC66b9b9B40a6474022D6962b6cAA001b5E3',
+    symbol: 'EBA',
+    name: 'Elpis Battle',
+    decimals: 18,
+    logoURI: 'https://i.imgur.com/Tzs373u.png',
+  },
+  '0xE81257d932280AE440B17AFc5f07C8A110D21432': {
+    chainId: 56,
+    address: '0xE81257d932280AE440B17AFc5f07C8A110D21432',
+    symbol: 'ZUKI',
+    name: 'ZUKI MOBA',
+    decimals: 18,
+    logoURI: 'https://zukimoba.com/images/logos/logo.png',
+  },
 }
 
 export default BSC_TOKEN_LIST
