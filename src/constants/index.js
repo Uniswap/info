bbbdc1166f728873f5ab413dceca323c1544f234
ianlapham <ianlapham@gmail.com>
2021-12-17 15:57:13 -0500

    update always tracked token pairs

--- src/constants/index.js
+++ src/constants/index.js
@@ -55,8 +55,8 @@ export const FEE_WARNING_TOKENS = ['0xd46ba6d942050d489dbd938a2c909a5d5039a161']
 
 export const UNTRACKED_COPY = 'Derived USD values may be inaccurate without liquid stablecoin or ETH pairings.'
 
-// tokens that should be tracked but arent due to lag in subgraph
-export const TRACKED_OVERRIDES = [
+// pairs that should be tracked but arent due to lag in subgraph
+export const TRACKED_OVERRIDES_PAIRS = [
   '0x9928e4046d7c6513326ccea028cd3e7a91c7590a',
   '0x87da823b6fc8eb8575a235a824690fda94674c88',
   '0xcd7989894bc033581532d2cd88da5db0a4b12859',
@@ -64,3 +64,7 @@ export const TRACKED_OVERRIDES = [
   '0x45804880de22913dafe09f4980848ece6ecbaf78',
   '0x709f7b10f22eb62b05913b59b92ddd372d4e2152',
 ]
+
+// tokens that should be tracked but arent due to lag in subgraph
+// all pairs that include token will be tracked
+export const TRACKED_OVERRIDES_TOKENS = ['0x956f47f50a910163d8bf957cf5846d573e7f87ca']
