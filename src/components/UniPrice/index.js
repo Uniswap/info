755431109b691030cc91900c30fdd3a8620890e9
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2022-05-03 17:17:06 +0300

    fix(ETH): set proper pair address for eth popup price

--- src/components/UniPrice/index.js
+++ src/components/UniPrice/index.js
@@ -24,9 +24,9 @@ function formatPercent(rawPercent) {
 }
 
 export default function UniPrice() {
-  const daiPair = usePairData('0xa478c2975ab1ea89e8196811f51a7b7ade33eb11')
-  const usdcPair = usePairData('0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc')
-  const usdtPair = usePairData('0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852')
+  const daiPair = usePairData('0xfbc24cd5dea5570f1d65022009f7ad9f7d3f8ade')
+  const usdcPair = usePairData('0xac3b98cb04a320c71a9929dffefe558daf217a79')
+  const usdtPair = usePairData('0xa029a744b4e44e22f68a1bb9a848caafbf6bb233')
 
   const totalLiquidity = useMemo(() => {
     return daiPair && usdcPair && usdtPair
