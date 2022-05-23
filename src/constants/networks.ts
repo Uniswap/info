f6c54bddc85f8841b7c8ebd11a72aa81a5e77af9
Nam Nguyen <hnamnguyencse@gmail.com>
2022-05-23 16:52:50 +0700

    change bttc block subgraph (#316)
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -342,7 +342,7 @@ export const NETWORKS_INFO: { [key in ChainId]: NETWORK_INFO } = {
     factoryAddress: '0xD9bfE9979e9CA4b2fe84bA5d4Cf963bBcB376974',
     subgraphName: 'dynamic-amm/kyberswap-bttc',
     subgraphUrls: ['https://bttc-graph.kyberengineering.io/subgraphs/name/kybernetwork/kyberswap-exchange-bttc'],
-    subgraphBlockUrl: 'https://bttc-graph.dev.kyberengineering.io/subgraphs/name/dynamic-amm/bttc-blocks',
+    subgraphBlockUrl: 'https://bttc-graph.kyberengineering.io/subgraphs/name/kybernetwork/bttc-blocks',
     etherscanUrl: 'https://bttcscan.com',
     kncAddress: '0x868fc5cb3367c4a43c350b85d5001acaf58a857e',
     wethAddress: '0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23',
