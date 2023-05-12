fb50984b6f2a71ee8e076e885f5574fb28bac5a7
Nicole Moiseyev <nicole.moiseyev3400@gmail.com>
2023-05-12 08:50:51 -0400

    Use xyzd endpoint

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,9 +2,11 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://localhost:2000' : 'https://api.n.xyz'
+
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2',
+    uri: `${BASE_URL}/api/v1/unidex/graphql`,
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
